37b84a9008d2d54c2a2c92eba6c4fa3abf6eca7f
Muxammadsodiq <120007009+MuxammadsodiqDev@users.noreply.github.com>
2024-04-09 10:02:42 +0500

    Update app.py

--- app.py
+++ app.py
@@ -17,12 +17,28 @@ if st.button("Funksiya kiritish qo'llanmasi"):
 #Berilgan funksiyani hisobberuvchi function
 def calculate_function(expression, x_value):
     try:
-        result = eval(expression, {'__builtins__': None}, {'x': x_value, 'sin': math.sin, 'cos': math.cos, 'tan': math.tan, 'e': math.exp, 'log': math.log})
+        result = eval(expression, {'__builtins__': None}, 
+                                                          {'x': x_value, 
+                                                           'e': math.exp,'pi':mat.pi,
+                                                           'sin': math.sin, 'arcsin': math.asin,'sh': math.sinh, 'arcsh': math.asinh,
+                                                           'cos': math.cos,  'arccos': math.acos,'ch': math.cosh,'arcch': math.acosh,
+                                                           'tan': math.tan,  'arctan': math.atan, 'th': math.tanh, 'arcth': math.atanh,
+                                                           'log': math.log, 'log2':math.log2,'log3': math.log3,'log4': math.log4,'log5': math.log5,
+                                                           'log6': math.log6,'log7': math.log7,'log8': math.log8,'log9': math.log9,'log10': math.log10
+                      })
         return result
         
     except Exception as e:
         return str(e)
 
+def calculate_function(expression, x_value):
+    try:
+        result = eval(expression, {'__builtins__': None}, {'x': x_value, 'sin': math.sin, 'cos': math.cos, 'tan': math.tan, 'e': math.exp, 'log': math.log})
+        return result
+        
+    except Exception as e:
+        return str(e)
+        
 #Foydalanuvchi tamonidan kiritilishi kerak
 func = st.text_input("Funksiya: ","x**2-9")
 func1 = st.text_input("Funksiyaning 1-tartibli hosilasi: ","2*x")
