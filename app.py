132395365d7a6025105ccff30796760cb587a542
Muxammadsodiq <120007009+MuxammadsodiqDev@users.noreply.github.com>
2024-04-27 19:35:46 +0500

    Update app.py

--- app.py
+++ app.py
@@ -53,7 +53,7 @@ b = st.number_input("oraliqni oxirgi qiymati: ")
 if st.button('Funksiya grafigi'):
     try:
         if func:
-            x = np.linspace(-np.inf, np.inf, 10)
+            x_values = np.arange(a, b, ϵ)
             y_values = [calculate_function(func, x) for x in x_values]
             y=0/x_values
     
