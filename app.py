74a2c9b02b6cb1c416b147a002ede103d1bb39c2
Muxammadsodiq <120007009+MuxammadsodiqDev@users.noreply.github.com>
2024-04-27 19:38:43 +0500

    Update app.py

--- app.py
+++ app.py
@@ -63,6 +63,11 @@ if st.button('Funksiya grafigi'):
             fig.update_layout(title=f'funksiya: {func}',
                             xaxis_title='x',
                             yaxis_title='y')
+            xaxis=dict(range=[x_min, x_max]),
+            yaxis=dict(range=[y_min, y_max]),
+            shapes=[
+                dict(type="line", x0=x_min, y0=0, x1=x_max, y1=0, line=dict(color="red", width=2)),
+                dict(type="line", x0=0, y0=y_min, x1=0, y1=y_max, line=dict(color="blue", width=2))
             st.plotly_chart(fig)
         else:
             error
