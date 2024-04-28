8a64b90360ee61dcec847229fa7597bd1382bd09
Muxammadsodiq <120007009+MuxammadsodiqDev@users.noreply.github.com>
2024-04-28 12:56:20 +0500

    Update app.py

--- app.py
+++ app.py
@@ -24,7 +24,7 @@ def calculate_function(expression, x_value):
                                                            'cos': math.cos,  'arccos': math.acos,'ch': math.cosh,'arcch': math.acosh,
                                                            'tan': math.tan,  'arctan': math.atan, 'th': math.tanh, 'arcth': math.atanh,
                                                            'ln': math.log, 'log2': lambda x: math.log(x, 2),'log3': lambda x: math.log(x,3),'log4':lambda x:math.log(x,4),'log5': math.log(5),
-                                                           'log6': math.log(6),'log7': math.log(7),'log8': math.log(8),'log9': math.log(9),'lg': math.log(10),'^':math.pow
+                                                           'log6': math.log(6),'log7': math.log(7),'log8': math.log(8),'log9': math.log(9),'lg': math.log(10),'^': lambda x:math.pow(x),"log":math.log
                                                            
                       })
         return result
