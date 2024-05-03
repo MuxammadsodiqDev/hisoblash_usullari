965036cd26c08ea6aed0502d522bbb87b24766bf
Muxammadsodiq <120007009+MuxammadsodiqDev@users.noreply.github.com>
2024-05-03 09:17:08 +0500

    Update app.py

--- app.py
+++ app.py
@@ -97,7 +97,7 @@ if st.button('1.Kesmani teng ikkiga bo\'lish usuli'):
             st.write('itaratsiya= ',itaratsiya)
             if len(itaratsiya_list)==len(c_list):
                 fig = go.Figure(data=[go.Bar(x=itaratsiya_list, y=c_list)])
-                fig.update_layout(title='x va itaratsiya', xaxis_title="itaratsiyalar", yaxis_title="x")
+                fig.update_layout(title='x va itaratsiya', xaxis_title="itaratsiyalar", yaxis_title="x",color='green')
                 st.plotly_chart(fig)
             else:
                 st.write('hato')
