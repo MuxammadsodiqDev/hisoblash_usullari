63d3fb657e02e17616eeb752e0d2960e6ba8a24a
Muxammadsodiq <120007009+MuxammadsodiqDev@users.noreply.github.com>
2024-05-06 13:16:54 +0500

    Update app.py

--- app.py
+++ app.py
@@ -71,8 +71,7 @@ b = st.number_input("b: ")
 
 option = st.selectbox(
     "Quyudaga usulni birini tanlang!",
-    ("Kesmani teng ikkiga bo'lish usul:", "Vatarlar usuli:", "Urunmalar usuli:","Urunma (madifiqatsiya) usuli:"),
-    index=None)
+    ("Kesmani teng ikkiga bo'lish usul:", "Vatarlar usuli:", "Urunmalar usuli:","Urunma (madifiqatsiya) usuli:")
 
 st.write("", option[0])
 
